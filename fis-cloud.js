ba89f263cab2aed9911335d748b96ce4e8eb419c
zhangying05 <zhangying05@baidu.com>
2013-08-05 16:18:09 +0800

    init project

---
+++ fis-cloud.js
@@ -0,0 +1,43 @@
+
+'use strict';
+
+var fis= module.exports = require('fis-cloud-kernal');
+
+fis.cli = {};
+fis.cli.name = 'fis-cloud';
+fis.cli.commander = null;
+fis.cli.info = fis.util.readJSON(__dirname + '/package.json');
+
+fis.cli.help = function(){
+    console.log(fis.cli.info);
+}
+fis.cli.version = function(){
+    console.log(fis.cli.info.version);
+}
+
+fis.cli.run = function(argv){
+    var first = argv[2];
+
+    if(first === '-h' || first === '--help'){
+        fis.cli.help();
+    } else if(first === '-v' || first === '--version') {
+        fis.cli.version();
+    } else if(first[0] === '-'){
+        fis.cli.help();
+    } else {
+        var commander = fis.cli.commander = require('commander');
+        var cmd = require('./lib/' + commander + '.js');
+        cmd.registry(
+            commander
+                .command(cmd.name || first)
+                .usage(cmd.usage)
+                .description(cmd.desc)
+        );
+        commander.parse(argv);
+    }
+}
+
+
+
+
+
